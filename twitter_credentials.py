0378e6b8a9af9e143caf08c3d15a649bb47af3ab
Chinmay Patil <72211393+ChinmayPatil11@users.noreply.github.com>
2021-05-25 19:02:44 +0530

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,5 @@
+api_key = 'Tuo3qoQlpDL8Rj3pgTCe9UJGo'
+api_secret = '6I253WfSRongspPRcmFyCKeooole7gLmuXjbnpIQ2Bqtie0dH2'
+OAuth_token = '3152248374-o9WW1lCJCWzcPuVB9ThKBWSFAMEQvsgfJ2QW5CL'
+OAuth_secret = 'JJwNkJNTzi1zE9ifWow4xGzOibQFV8OEjI0HhiLhdETsZ'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOpFNAEAAAAAMfK%2BQklg7KRkOovoIUKUIXc8CkE%3DTRJ20P75t8Y7zSHkOJj2dcazX04cktk8nHbcLFTrUqqlT24gCo'
