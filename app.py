f063787453b82ee68c60e0a8b6b637f71b1ad108
Chinmay Patil <chinmaypatil2000@gmail.com>
2021-05-26 11:09:39 +0530

    added files

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import flask
 from flask import Flask, render_template, request, url_for, redirect
-import h5py
 from tweet_extractor import get_related_tweets
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from keras.models import load_model
 from clean_data import clean, tokenize, make_predict, remove_usernames, remove_hashtags, remove_emails,remove_urls, remove_numbers, remove_punctuation, remove_stopwords, lemmatizer
-
+from entity_finder import entity_recognizer
 
 app = Flask(__name__)
 #model = h5py.File('classification_model.h5','r+')
 model = load_model('classification_model.h5')
 global tweets
+global dictionary
 
 def request_results(name):
     tweets = get_related_tweets(name)
-    #tweets['tweet_text'] = tweets['tweet_text'].apply(lambda x: clean(x))
+    print(tweets['tweet_text'])
     tweets['tweet_text'] = tweets['tweet_text'].apply(lambda x: remove_usernames(x))
     tweets['tweet_text'] = tweets['tweet_text'].apply(lambda x: remove_hashtags(x))
     tweets['tweet_text'] = tweets['tweet_text'].apply(lambda x: remove_emails(x))
@@ -27,11 +27,13 @@ def request_results(name):
     tweets['length'] = tweets['tweet_text'].apply(lambda x:len(x))
     tweets = tweets[tweets['length'] < 200]
     text_sequences_to_predict = tokenize(tweets['tweet_text'])
-    preds = model.predict(text_sequences_to_predict) #tweets['predictions']
+    preds = model.predict(text_sequences_to_predict)
+    preds = list(preds)
     preds = make_predict(preds)
     tweets['prediction'] = preds
     tweets['prediction'] = tweets['prediction'].replace({0:'negative',1:'positive'})
-    return (tweets)
+
+    return tweets
 
 @app.route("/")
 def home():
@@ -47,11 +49,13 @@ def get_data(): #name
 @app.route('/success/<name>')
 def success(name):
     tweets = request_results(name)
-    #no_of_tweets = tweets['prediction'].value_counts()
-    #plt.bar(no_of_tweets.index, no_of_tweets)
-    #plt.savefig('/static/images/plot.png')
     output_tweets = tweets.drop(['created_at','tweet_id','length'],axis=1)
-    return render_template('success.html',tables=[output_tweets.to_html(classes='data')], titles=tweets.columns.values)
+    entities = entity_recognizer(output_tweets)
+    dictionary = dict(zip(tweets['tweet_text'],tweets['prediction']))
+    #print(entities)
+    #return render_template('success.html',tables=[output_tweets.to_html(classes='data')]
+    #                        , titles=tweets.columns.values,all_entities=dictionary)
+    return render_template('success.html',lines = dictionary)
 
 if __name__ == '__main__':
     app.run(debug=True)
