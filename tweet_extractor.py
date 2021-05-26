e51b19d9449797c8f7a6aba1172e841bb1a9101e
Chinmay Patil <chinmaypatil2000@gmail.com>
2021-05-26 18:23:41 +0530

    Modified files

--- tweet_extractor.py
+++ tweet_extractor.py
@@ -12,14 +12,15 @@ api = API(auth, wait_on_rate_limit = True)
 def get_related_tweets(query):
 
     tweets_list = []
+    query = query + '-filter:retweets'
     count = 10
     try:
-        for tweet in api.search(q='covid',lang='en',count=count):
+        for tweet in api.search(q=query,lang='en',count=count):
             #print(tweet.text)
             tweets_list.append({'created_at': tweet.created_at,
                                 'tweet_id': tweet.id,
                                 'tweet_text': tweet.text})
-
+    
         return pd.DataFrame.from_dict(tweets_list)
 
     except BaseException as e:
